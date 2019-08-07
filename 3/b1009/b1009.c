d3f1822742daaced311ed10c2d3d147a3358d4b9
李明燮 <limingxie@limingxie-pc.home.e-kmall.com>
2019-08-07 08:55:23 +0800

    b1009

---
+++ 3/b1009/b1009.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char v[50][100];
+    char str[100];
+    gets(str);
+    int len = strlen(str);
+    int num = 0, h = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] != ' ')
+        {
+            v[num][h++] = str[i];
+        }
+        else
+        {
+            num++;
+            h = 0;
+        }
+    }
+
+    for (int i = num; i >= 0; i--)
+    {
+        printf("%s", v[i]);
+        if (i != 0)
+            printf(" ");
+    }
+    return 0;
+}
+
+//gcc ./b1009/*.c -o a.out && ./a.out
