c75cfc435bb08486f2bfc03763f8ac201438437f
李明燮 <limingxie@limingxie-pc.home.e-kmall.com>
2019-08-20 10:18:06 +0800

    a1082

--- a/a1082_go/a1082.go
+++ a/a1082_go/a1082.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
+	result := ""
 	num := []string{"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"}
 	// wei := []string{"Shi", "Bai", "Qian", "Wan", "Yi"}
 
@@ -15,72 +17,78 @@ func main() {
 	scanner.Scan()
 	str := scanner.Text()
 
+	if str == "0" || str == "-0" {
+		fmt.Print("ling")
+		return
+	}
+
 	slen := len(str)
 
 	left := 0
-	// right := slen - 1
 	if str[0] == '-' {
-		fmt.Print("Fu ")
+		result += "Fu "
 		left++
 	}
 
-	rr := make([]string, 10)
+	var wflag bool = true
+
+	if slen-left >= 9 {
+		if str[0+left] != '0' && str[1+left] == '0' && str[2+left] == '0' && str[3+left] == '0' && str[4+left] == '0' {
+			wflag = false
+		}
+	}
+
+	rr := make([]string, slen)
 	no := 0
-	for i := slen - 1; i >= 0; i-- {
+	for i := slen - 1; i >= left; i-- {
 		n, _ := strconv.Atoi(string(str[i]))
 		rr[no] = num[n]
 		no++
 	}
 
-	for i := 0; i < slen-1; i++ {
+	for i := 0; i < slen-left; i++ {
 		if i == 0 && rr[i] == "ling" {
 			rr[i] = "a"
-		} else if rr[i] == "ling" {
-			rr[i] = "a"
-		} else {
-			break
-		}
-	}
-
-	for i := 0; i < slen-1; i++ {
-		if i != 0 && rr[i] == "ling" && rr[i-1] != "ling" {
+		} else if i != 0 && rr[i] == "ling" && rr[i-1] != "ling" && rr[i-1] != "b" && rr[i-1] != "a" {
 			rr[i] = "b"
-		} else if i != 0 && rr[i] == "ling" && rr[i-1] == "b" {
+		} else if i != 0 && rr[i] == "ling" && (rr[i-1] == "b" || rr[i-1] == "a") {
 			rr[i] = "a"
-		} else {
-			break
 		}
 	}
 
-	vv := make([]string, 50)
+	vv := make([]string, 100)
 	vi := 0
-	for i := 0; i < slen; i++ {
-		if i == 8 && rr[i] != "a" {
+	for i := 0; i < slen-left; i++ {
+		if i == 8 && rr[i] != "a" && rr[i] != "b" {
 			vv[vi] = "Yi"
 			vi++
 			vv[vi] = rr[i]
 			vi++
-		} else if i%5 == 1 && rr[i] != "a" {
+		} else if (i-1)%4 == 0 && rr[i] != "a" && rr[i] != "b" {
 			vv[vi] = "Shi"
 			vi++
 			vv[vi] = rr[i]
 			vi++
-		} else if i%5 == 2 && rr[i] != "a" {
+		} else if (i-1)%4 == 1 && rr[i] != "a" && rr[i] != "b" {
 			vv[vi] = "Bai"
 			vi++
 			vv[vi] = rr[i]
 			vi++
-		} else if i%5 == 3 && rr[i] != "a" {
+		} else if (i-1)%4 == 2 && rr[i] != "a" && rr[i] != "b" {
 			vv[vi] = "Qian"
 			vi++
 			vv[vi] = rr[i]
 			vi++
-		} else if i%5 == 4 && rr[i] != "a" {
-			vv[vi] = "Wan"
-			vi++
-			vv[vi] = rr[i]
-			vi++
-		} else if i%5 == 0 && i != 0 && rr[i] != "a" {
+		} else if (i-1)%4 == 3 {
+			if wflag {
+				vv[vi] = "Wan"
+				vi++
+			}
+			if rr[i] != "b" && rr[i] != "a" {
+				vv[vi] = rr[i]
+				vi++
+			}
+		} else if (i-1)%4 == 0 && i != 0 && rr[i] != "a" && rr[i] != "b" {
 			vv[vi] = "Shi"
 			vi++
 			vv[vi] = rr[i]
@@ -92,15 +100,17 @@ func main() {
 
 	}
 
-	vlen := len(vv)
-
-	for i := vlen - 1; i >= 0; i-- {
+	for i := 99; i >= 0; i-- {
 		if vv[i] == "b" {
-			fmt.Print("ling ")
-		} else if vv[i] != "a" {
-			fmt.Print(vv[i], " ")
+			result += "ling "
+		} else if vv[i] != "" && vv[i] != "a" {
+			result += vv[i]
+			if i != 0 {
+				result += " "
+			}
 		}
 	}
+	fmt.Println(strings.TrimSpace(result))
 }
 
-//g++ a1082/*.cpp -o a.out && ./a.out
+//go build && ./a1082_go
