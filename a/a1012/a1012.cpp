bf119103c6b4f9ce807263b0416272f5ff786853
李明燮 <limingxie@limingxie-pc.home.e-kmall.com>
2019-08-23 10:57:49 +0800

    a1012

--- a/a1012/a1012.cpp
+++ a/a1012/a1012.cpp
@@ -6,118 +6,163 @@ using namespace std;
 struct Student
 {
     int id;
-    int C, M, E, A;
-    int CC, MM, EE, AA;
+    int C, M, E;
+    double A;
+    int C_rank, M_rank, E_rank, A_rank;
+};
 
-} stu[100010];
-
-void ss_print(Student s1)
+void stu_print(Student s)
 {
-    char g[1];
-    int rank;
-    strcpy(g, "A");
-    rank = s1.AA;
-    if (s1.CC < rank)
+    int highest_rank = s.A_rank;
+    char highest_rank_subject = 'A';
+    if (s.C_rank < highest_rank)
     {
-        rank = s1.CC;
-        strcpy(g, "C");
+        highest_rank = s.C_rank;
+        highest_rank_subject = 'C';
     }
-    else if (s1.MM < rank)
+    if (s.M_rank < highest_rank)
     {
-        rank = s1.MM;
-        strcpy(g, "M");
+        highest_rank = s.M_rank;
+        highest_rank_subject = 'M';
     }
-    else if (s1.EE < rank)
+    if (s.E_rank < highest_rank)
     {
-        rank = s1.EE;
-        strcpy(g, "E");
+        highest_rank = s.E_rank;
+        highest_rank_subject = 'E';
     }
-    printf("%d %s\n", rank, g);
+    printf("%d %c\n", highest_rank, highest_rank_subject);
 }
 
 bool cmpC(Student s1, Student s2)
 {
-    return s1.C > s2.C;
+    if (s1.C != s2.C)
+    {
+        return s1.C > s2.C;
+    }
+    else
+    {
+        return s1.id < s2.id;
+    }
 }
 bool cmpM(Student s1, Student s2)
 {
-    return s1.M > s2.M;
+    if (s1.M != s2.M)
+    {
+        return s1.M > s2.M;
+    }
+    else
+    {
+        return s1.id < s2.id;
+    }
 }
 bool cmpE(Student s1, Student s2)
 {
-    return s1.E > s2.E;
+    if (s1.E != s2.E)
+    {
+        return s1.E > s2.E;
+    }
+    else
+    {
+        return s1.id < s2.id;
+    }
 }
 bool cmpA(Student s1, Student s2)
 {
-    return s1.A > s2.A;
+    if (s1.A != s2.A)
+    {
+        return s1.A > s2.A;
+    }
+    else
+    {
+        return s1.id < s2.id;
+    }
 }
 int main()
 {
     int n, m;
     scanf("%d %d", &n, &m);
 
+    Student stu[n];
     for (int i = 0; i < n; i++)
     {
         scanf("%d %d %d %d", &stu[i].id, &stu[i].C, &stu[i].M, &stu[i].E);
-        stu[i].A = (stu[i].C + stu[i].M + stu[i].E) / 3;
-    }
-
-    sort(stu, stu + n, cmpC);
-    for (int i = 0; i < n; i++)
-    {
-        if (i != 0 && stu[i].C == stu[i - 1].C)
-        {
-            stu[i].CC = stu[i - 1].CC;
-        }
-        stu[i].CC = i + 1;
-    }
-
-    sort(stu, stu + n, cmpM);
-    for (int i = 0; i < n; i++)
-    {
-        if (i != 0 && stu[i].M == stu[i - 1].M)
-        {
-            stu[i].MM = stu[i - 1].MM;
-        }
-        stu[i].MM = i + 1;
-    }
-
-    sort(stu, stu + n, cmpE);
-    for (int i = 0; i < n; i++)
-    {
-        if (i != 0 && stu[i].E == stu[i - 1].E)
-        {
-            stu[i].EE = stu[i - 1].EE;
-        }
-        stu[i].EE = i + 1;
+        stu[i].A = double(stu[i].C + stu[i].M + stu[i].E) / 3;
     }
 
     sort(stu, stu + n, cmpA);
     for (int i = 0; i < n; i++)
     {
         if (i != 0 && stu[i].A == stu[i - 1].A)
-        {
-            stu[i].AA = stu[i - 1].AA;
-        }
-        stu[i].AA = i + 1;
+            stu[i].A_rank = stu[i - 1].A_rank;
+        else
+            stu[i].A_rank = i + 1;
+    }
+
+    sort(stu, stu + n, cmpC);
+    for (int i = 0; i < n; i++)
+    {
+        if (i != 0 && stu[i].C == stu[i - 1].C)
+            stu[i].C_rank = stu[i - 1].C_rank;
+        else
+            stu[i].C_rank = i + 1;
+    }
+
+    sort(stu, stu + n, cmpM);
+    for (int i = 0; i < n; i++)
+    {
+        if (i != 0 && stu[i].M == stu[i - 1].M)
+            stu[i].M_rank = stu[i - 1].M_rank;
+        else
+            stu[i].M_rank = i + 1;
+    }
+
+    sort(stu, stu + n, cmpE);
+    for (int i = 0; i < n; i++)
+    {
+        if (i != 0 && stu[i].E == stu[i - 1].E)
+            stu[i].E_rank = stu[i - 1].E_rank;
+        else
+            stu[i].E_rank = i + 1;
     }
 
     int id;
+    bool is_found = false;
     for (int i = 0; i < m; i++)
     {
         scanf("%d", &id);
-        printf("======%d======\n", i);
         for (int j = 0; j < n; j++)
         {
             if (stu[j].id == id)
             {
-                ss_print(stu[j]);
+                stu_print(stu[j]);
+                is_found = true;
+                break;
             }
         }
-        printf("======%d======\n", i);
+        if (!is_found)
+        {
+            printf("N/A\n");
+        }
+        is_found=false;
     }
 
     return 0;
 }
 
 //g++ a1012/*.cpp -o a.out && ./a.out
+
+/*
+5 6
+310101 98 85 88
+310102 70 95 88
+310103 82 87 94
+310104 91 91 91
+310105 85 90 90
+310101
+310102
+310103
+310104
+310105
+999999
+
+*/
